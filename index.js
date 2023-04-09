c67f8ed03d6563ab894fceb064aa088724a2ee6f
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-04-09 08:42:59 +0530

    done

--- index.js
+++ index.js
@@ -12,15 +12,27 @@
     function display(data){
     data.map(function(elem){
         var div=document.createElement("div");
+        div.setAttribute("class","card");
+        div.style.width="18rem";
         var image=document.createElement("img");
         image.src=elem.ImageUrl;
-        var h2=document.createElement("h2");
-        h2.innerText=elem.Name;
+        image.setAttribute("class","card-image-top");
+        var div1=document.createElement("div");
+        div1.setAttribute("class","card-body");
+        
+       
+        var h5=document.createElement("h5");
+        h5.setAttribute("class","card-title");
+        h5.innerText=elem.Name;
         var p=document.createElement("p");
+        p.setAttribute('class',"card-text");
         p.innerText=elem.ShortDesc;
         var button=document.createElement("button");
-        button.innerHTML="View";
-        div.append(image,h2,p,button);
-        document.getElementById("Container").append(div);
+        button.setAttribute("class","btn btn-primary");
+        button.style.backgroundColor="none"
+        button.innerText="View";
+        div1.append(h5,p,button);
+        div.append(image,div1);
+        document.getElementById("Cards").append(div);
     })
   }
